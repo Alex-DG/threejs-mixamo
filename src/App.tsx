5859c7d84512c5e9028fdcb4a522417c7f93c5f9
Alexandre Di Guida <alexandre.diguida@gmail.com>
2020-06-02 13:50:20 +0100

    🎉 First commit
    Character loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,187 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+import React, { useState, useEffect, useRef } from 'react'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+
+import * as THREE from 'three'
+
+import styled from 'styled-components'
+
+// @ts-ignore
+// import eve from './models/fbx/eve_j_gonzales.fbx'
+// @ts-ignore
+import kachujin from './models/fbx/kachujin_g_rosales.fbx'
+// @ts-ignore
+// import sambaDancing from './models/fbx/samba-dancing.fbx'
+
+const LoadingWrapper = styled.div`
+  min-height: 100vh;
+
+  background-color: transparent;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const LoadingText = styled.h1`
+  color: white;
+`
+
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  padding: 15px;
+
+  position: relative;
+
+  background-color: grey;
+
+  border-radius: 4px;
+
+  margin: 0 auto;
+
+  z-index: 0;
+`
+
+const StyledCanvas = styled.canvas`
+  position: absolute;
+  z-index: -1;
+  min-height: 100vh;
+
+  background-color: transparent;
+`
+
+const App = () => {
+  const [loading, setLoading] = useState(true)
+
+  const canvasRef = useRef<HTMLCanvasElement>({
+    // @ts-ignore
+    current: { clientWidth: 0, clientHeight: 0 },
+  })
+
+  let renderer: THREE.WebGLRenderer
+  let camera: THREE.PerspectiveCamera
+  let scene: THREE.Scene
+
+  const sceneAdd = (item: THREE.Object3D) => scene.add(item)
+
+  const createCam = () => {
+    camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      1,
+      2000,
+    )
+    camera.position.set(100, 200, 300)
+  }
+
+  const createScene = () => {
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xa0a0a0)
+    scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000)
+  }
+
+  const createLight = () => {
+    let hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x444444)
+    hemisphereLight.position.set(0, 200, 0)
+    sceneAdd(hemisphereLight)
+
+    let directionalLight = new THREE.DirectionalLight(0xffffff)
+    directionalLight.position.set(0, 200, 100)
+    directionalLight.castShadow = true
+    directionalLight.shadow.camera.top = 180
+    directionalLight.shadow.camera.bottom = -100
+    directionalLight.shadow.camera.left = -120
+    directionalLight.shadow.camera.right = 120
+    sceneAdd(directionalLight)
+  }
+
+  const createGround = () => {
+    let mesh = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(2000, 2000),
+      new THREE.MeshPhongMaterial({
+        color: 0x999999,
+        depthWrite: false,
+      }),
+    )
+    mesh.rotation.x = -Math.PI / 2
+    mesh.receiveShadow = true
+    sceneAdd(mesh)
+
+    let grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000)
+    // @ts-ignore
+    grid.material.opacity = 0.2
+    // @ts-ignore
+    grid.material.transparent = true
+    sceneAdd(grid)
+  }
+
+  const init = () => {
+    createCam()
+    createScene()
+    createLight()
+    createGround()
+
+    const loader = new FBXLoader()
+
+    loader.load(
+      kachujin,
+      (object: any) => {
+        object.traverse((child: any) => {
+          // @ts-ignore
+          if (child.isMesh) {
+            child.castShadow = true
+            child.receiveShadow = true
+          }
+        })
+
+        sceneAdd(object)
+
+        setTimeout(() => setLoading(false), 1000)
+      },
+      (event: ProgressEvent<EventTarget>) => console.log('onProgress ->', { event }),
+      (error: ErrorEvent) => console.log('error = ', { error }),
+    )
+
+    renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      canvas: canvasRef.current,
+    })
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.shadowMap.enabled = true
+  }
+
+  const animate = () => {
+    requestAnimationFrame(animate)
+
+    renderer.render(scene, camera)
+  }
+
+  useEffect(
+    () => {
+      init()
+      animate()
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <StyledCanvas ref={canvasRef} />
+      <LoadingWrapper>
+        {loading && (
+          <LoadingContainer className="loading">
+            <LoadingText>Loading...</LoadingText>
+          </LoadingContainer>
+        )}
+      </LoadingWrapper>
+    </>
+  )
 }
 
-export default App;
+export default App
